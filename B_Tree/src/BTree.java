b426ed93354d48e895665ae04a40ab30cecba9c7
ChrisC-7 <jxwork0211@gmail.com>
2025-05-12 10:23:07 -0700

    Basic class
    Add the class we need to use

---
+++ B_Tree/src/BTree.java
@@ -0,0 +1,17 @@
+public class BTree {
+
+
+
+
+
+
+    private class Node{
+        private int order;
+        private int[] keys;
+        private Node[] children;
+        private int size;
+
+
+
+    }
+}
